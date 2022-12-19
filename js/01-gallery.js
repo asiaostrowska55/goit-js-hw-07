ca3feec7c1cd6bb58e86a01bab26476fbfce8d79
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-19 17:38:02 +0100

    task1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+"use strict";
 
-console.log(galleryItems);
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+const gallery = document.querySelector("div.gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (image) => `<div class="gallery__item">
+<a class="gallery__link" href="${image.original}">
+  <img
+    class="gallery__image"
+    src="${image.preview}"
+    data-source="${image.original}"
+    alt="${image.description}"
+  />
+</a>
+</div>`
+  )
+  .join("");
+
+gallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+gallery.addEventListener("click", (select) => {
+  select.preventDefault();
+  const instance = basicLightbox.create(`
+    <img src="${select.target.dataset.source}"/>
+`);
+  instance.show();
+
+  document.addEventListener("keydown", (close) => {
+    if (close.key !== "ESC") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,15 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const gallery = document.querySelector("ul.gallery");
+
+const galleryMarkup = galleryItems
+  .map(
+    (image) => `<div class="gallery__item">
+<a class="gallery__item" href="${image.original}">
+  <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+</a>
+</div>`
+  )
+  .join("");
 
 console.log(galleryItems);
