47b27d64c4c9968a5f81c6f4441f8dd4827bf375
asiaostrowska55 <110023821+asiaostrowska55@users.noreply.github.com>
2022-12-20 18:15:34 +0100

    ostatnie poprawki

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,6 +1,6 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
-const gallery = document.querySelector("ul.gallery");
+const imgGallery = document.querySelector(".gallery");
 
 const galleryMarkup = galleryItems
   .map(
@@ -12,4 +12,16 @@ const galleryMarkup = galleryItems
   )
   .join("");
 
-console.log(galleryItems);
+imgGallery.insertAdjacentHTML("beforeend", galleryMarkup);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+  captionsData: "alt",
+});
+
+// poniższy kod ukrywa widoczny kawałek boxshadow w przeglądarce //
+let shadowBox = document.querySelectorAll("a.gallery__item");
+let arr = Array.from(shadowBox);
+arr.forEach((el) => {
+  el.style.display = "contents";
+});
